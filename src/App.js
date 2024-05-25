957c34cd24753ac5013cc334d040a3c90d9f2a3c
Kailash Somanathan <kailashnathan@hotmail.co.uk>
2024-05-25 11:42:41 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { deckArray } from './components/deck';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+console.log(deckArray[0]);
+
+export default App;
